51bb6a6b8c5ad92ec9d5190c924fcd1a9e7c34c6
Kehitt <devstealth@gmail.com>
2020-01-31 15:06:35 +0300

    Refactor email_validator to introduce parse() method

--- src/email_validator.erl
+++ src/email_validator.erl
@@ -1,65 +1,98 @@
 -module(email_validator).
 
 -export([validate/1]).
+-export([parse/1]).
 
 -define(DOMAIN_MAX_SIZE, 255).
 -define(LOCAL_MAX_SIZE,  64).
 -define(ADDR_MAX_SIZE, ?LOCAL_MAX_SIZE + ?DOMAIN_MAX_SIZE + 1).
 
+-type parse_result() :: #{
+    'local-part' := binary(),
+    'domain'     := binary()
+}.
+
 -spec validate(binary() | string()) ->
     ok | {error, term()}.
 
-validate(Address) when is_list(Address) ->
+validate(Address) ->
+    case parse(Address) of
+        {ok, _Result} ->
+            ok;
+        Error ->
+            Error
+    end.
+
+-spec parse(binary() | string()) ->
+    {ok, parse_result()} | {error, term()}.
+
+parse(Address) when is_list(Address) ->
     case unicode:characters_to_binary(Address) of
         Result when is_binary(Result) ->
-            validate(Result);
+            parse(Result);
         {error, Encoded, Rest} ->
             {error, {unicode, {invalid, Encoded, Rest}}};
         {incomplete, Encoded, Rest} ->
             {error, {unicode, {incomplete, Encoded, Rest}}}
     end;
-validate(Address) when is_binary(Address) ->
-    validate_addr(Address);
-validate(Address) ->
+parse(Address) when is_binary(Address) ->
+    parse_addr(Address);
+parse(Address) ->
     {error, {not_an_address, Address}}.
 
 %%
 
 % As per https://tools.ietf.org/html/rfc5321#section-4.5.3.1
 % we need to validate maximum sizes
-validate_addr(Address) when byte_size(Address) =< ?ADDR_MAX_SIZE ->
-    case split_local_domain(Address) of
-        [Local, Domain] ->
-            validate_local_domain(Local, Domain);
-        _ ->
-            {error, {not_an_address, Address}}
+parse_addr(Address) when byte_size(Address) =< ?ADDR_MAX_SIZE ->
+    case parse_rule('mailbox', Address) of
+        {ok, ParsedAddr} ->
+            validate_sizes(ParsedAddr);
+        Error ->
+            Error
     end;
-validate_addr(Address) ->
+parse_addr(Address) ->
     {error, {'mailbox', {too_big, Address}}}.
 
-split_local_domain(Address) ->
-    string:split(Address, "@", trailing).
-
-validate_local_domain(Local, Domain) ->
-    case validate_local(Local) of
-        ok ->
-            validate_domain(Domain);
-        {error, _} = Error ->
-            Error
+validate_sizes(#{'local-part' := Local, 'domain' := Domain} = ParsedAddr) ->
+    case validate_part_sizes(Local, Domain) of
+        ok -> {ok, ParsedAddr};
+        Error -> Error
     end.
 
-validate_local(Local) when byte_size(Local) =< ?LOCAL_MAX_SIZE ->
-    validate_rule('local-part', Local);
-validate_local(Local) ->
+validate_part_sizes(Local, Domain) ->
+    case validate_local_size(Local) of
+        ok -> validate_domain_size(Domain);
+        Error -> Error
+    end.
+
+validate_local_size(Local) when byte_size(Local) =< ?LOCAL_MAX_SIZE ->
+    ok;
+validate_local_size(Local) ->
     {error, {'local-part', {too_big, Local}}}.
 
-validate_domain(Domain) when byte_size(Domain) =< ?DOMAIN_MAX_SIZE ->
-    validate_rule('domain', Domain);
-validate_domain(Domain) ->
+validate_domain_size(Domain) when byte_size(Domain) =< ?DOMAIN_MAX_SIZE ->
+    ok;
+validate_domain_size(Domain) ->
     {error, {'domain', {too_big, Domain}}}.
 
-validate_rule(Rule, Data) ->
+parse_rule(Rule, Data) ->
     case email_validator_abnf:decode(Rule, Data) of
-        {ok, _, <<>>} -> ok;
-        _ -> {error, {Rule, {parse_failed, Data}}}
+        {ok, ParseResult, <<>>} ->
+            {ok, decode_parse_result(ParseResult)};
+        _Error ->
+            {error, {Rule, {parse_failed, Data}}}
     end.
+
+decode_parse_result([Local, $@, Domain]) ->
+    #{
+        'local-part' => decode_local_part(Local),
+        'domain'     => decode_domain(Domain)
+    }.
+
+%TODO: More result processing, normalize IP addresses, etc
+decode_local_part(Local) ->
+    unicode:characters_to_binary(lists:flatten(Local)).
+
+decode_domain(Domain) ->
+    unicode:characters_to_binary(lists:flatten(Domain)).
